03545f1a74f2ffadaf56f1feee3a37c3856b8222
Shawn Clary <shawnclary@Shawns-MacBook-Pro.local>
2021-11-27 22:48:01 -0500

    mostly done

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react"
+import styled from "styled-components/macro"
 
-import SHOES from '../../data';
-import ShoeCard from '../ShoeCard';
+import SHOES from "../../data"
+import ShoeCard from "../ShoeCard"
 
 const ShoeGrid = () => {
-  return (
-    <Wrapper>
-      {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
-      ))}
-    </Wrapper>
-  );
-};
+	return (
+		<Wrapper>
+			{SHOES.map((shoe) => (
+				<ShoeCard key={shoe.slug} {...shoe} />
+			))}
+		</Wrapper>
+	)
+}
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	display: flex;
+	justify-content: space-around;
+	gap: 24px;
+	flex-wrap: wrap;
+`
 
-export default ShoeGrid;
+export default ShoeGrid
